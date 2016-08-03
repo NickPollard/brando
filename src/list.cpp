ea8eb13feef1a985c9c6e09c536ba078ff46a6f3
Nick Pollard <nickedwardpollard@gmail.com>
2016-08-03 22:20:51 +0100

    Remove unneeded src files.

--- src/list.cpp
+++
@@ -1,9 +0,0 @@
-// list.cpp
-#include "list.h"
-
-	void test() {
-		auto emp = List<int>::empty();
-		auto a = List<int>::cons(1, emp);
-		auto b = List<int>::cons(2, a);
-		printf("%d\n", b->head());
-	}
